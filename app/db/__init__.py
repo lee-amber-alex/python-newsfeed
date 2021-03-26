e2c6b548510b00250a5abac11077e088b24e5628
Amber Lee <lee.amber.alex@gmail.com>
2021-03-26 14:38:49 -0700

    Removed unused comments.

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -6,9 +6,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 # connect to database using env variable
 
 #manages overall connection to db
+
 engine = create_engine(getenv('DB_URL'), echo=True, pool_size=20, max_overflow=0)
 
 #generates tem connections for CREATE, READ, UPDATE, DELETE
